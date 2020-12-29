f36c9deec0ce1cc64bea11087f435930bb5017bd
CPernet <wamcyril@gmail.com>
2020-12-29 10:15:25 +0000

    update RMSE

--- limo_test_irls.m
+++ limo_test_irls.m
@@ -3,12 +3,14 @@
 % this script imnplement a series of simple Monte Carlo simulation testing
 % that the IRLS weights data as it should comparing with robustfit (this is
 % not exactly the same implementation but both use a bisquare function and
-% the same tuning contant, so on average this must be very similar
+% the same tuning contant, so this must be very similar.
 
 NMC = 1000;
 sample_sizes = 10:10:100;
+warning off % because robustfit will moan at rank deficiency
 
 %% regression
+MRMSE = NaN(length(sample_sizes),2);
 parfor N = 1:length(sample_sizes)
     RMSE = NaN(2,NMC);
     for MC = 1:NMC
@@ -19,27 +21,26 @@ parfor N = 1:length(sample_sizes)
         % if we use limo_glm with IRLS
         % ----------------------------
         b = limo_IRLS(X,Y);
-        RMSE(1,MC) = sqrt(mean((Y(1,:)'-X*b).^2));
+        RMSE(1,MC) = sqrt(mean((Y-X*b).^2));
         
         % compare to robustfit
         % --------------------
-        B = robustfit(X(:,1) ,Y,'bisquare',4.685);
-        RMSE(2,MC) = sqrt(mean((Y(1,:)'-X*flipud(B)).^2));
+        [B,stats] = robustfit(X(:,1) ,Y,'bisquare',4.685);
+        RMSE(2,MC) = sqrt(mean(stats.resid.^2));
     end
     MRMSE(N,:) = mean(RMSE,2);
 end
 
 figure('Name','RMSE')
-subplot(1,2,1);
+subplot(1,3,1);
 plot(sample_sizes,MRMSE,'LineWidth',2);
-title(sprintf('Regression design\nAverage RMSE over %g Monte Carlo per samples sizes',NMC))
+title(sprintf('Average RMSE - Regression design\n %g Monte Carlo per samples sizes',NMC))
 grid on; box on; xlabel('Sample Size'); ylabel('RMSE'); legend('limo irls','robustfit')
 
-%% ANCOVA
-
-warning off % because robustfit will moan at rank deficiency
+%% ANOVA/ANCOVA
+MRMSE = NaN(length(sample_sizes),4);
 parfor N = 1:length(sample_sizes)
-    RMSE = NaN(2,NMC);
+    RMSE = NaN(4,NMC);
     for MC = 1:1000
         r   = randn(sample_sizes(N)*3,4);
         Y   = r(1:sample_sizes(N),1:3);
@@ -49,19 +50,31 @@ parfor N = 1:length(sample_sizes)
         
         % if we use limo_glm with IRLS
         % ----------------------------
+        b = limo_IRLS([X(:,[1 2 3]) X(:,5)],Y);
+        RMSE(1,MC) = sqrt(mean((Y-[X(:,[1 2 3]) X(:,5)]*b).^2));
         b = limo_IRLS(X,Y);
-        RMSE(1,MC) = sqrt(mean((Y(1,:)'-X*b).^2));
+        RMSE(3,MC) = sqrt(mean((Y-X*b).^2));
         
         % compare to robustfit
         % --------------------
-        B = robustfit(X(:,1:4) ,Y,'bisquare',4.685);
-        RMSE(2,MC) = sqrt(mean((Y(1,:)'-X*flipud(B)).^2));
+        [B,stats] = robustfit(X(:,1:3) ,Y,'bisquare',4.685);
+        %Cat = X(:,1:2); Cat(find(X(:,3)),:)=-1;
+        %[B,stats] = robustfit(Cat ,Y,'bisquare',4.685);
+        RMSE(2,MC) = sqrt(mean(stats.resid.^2));
+        [B,stats] = robustfit(X(:,1:4) ,Y,'bisquare',4.685);
+        %[B,stats] = robustfit([Cat X(:,4)] ,Y,'bisquare',4.685);
+        RMSE(4,MC) = sqrt(mean(stats.resid.^2));
     end
     MRMSE(N,:) = mean(RMSE,2);
 end
 warning on
 
-subplot(1,2,2);
-plot(sample_sizes,MRMSE,'LineWidth',2);
-title(sprintf('ANCOVA design\nAverage RMSE over %g Monte Carlo per samples sizes',NMC))
+subplot(1,3,2);
+plot(sample_sizes,MRMSE(:,[1 2]),'LineWidth',2);
+title(sprintf('Average RMSE - ANOVA design\n %g Monte Carlo per samples sizes',NMC))
+grid on; box on; xlabel('Sample Size'); ylabel('RMSE'); legend('limo irls','robustfit')
+
+subplot(1,3,3);
+plot(sample_sizes,MRMSE(:,[3 4]),'LineWidth',2);
+title(sprintf('Average RMSE - ANCOVA design\n %g Monte Carlo per samples sizes',NMC))
 grid on; box on; xlabel('Sample Size'); ylabel('RMSE'); legend('limo irls','robustfit')
